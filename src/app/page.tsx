6d0c46c8a808a9320448d876ebc9de172651814b
Yul Jeon <dalbitstream@gmail.com>
2024-05-27 00:26:30 +0900

    feat: display assignment list on home page using cards with order, title, and description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,56 @@
+import Header from "@/components/header";
+import Link from "next/link";
+
+const assignments = [
+  {
+    order: 1,
+    title: "상품 목록 만들기",
+    description: "object 형식의 데이터를 이용해서 상품 목록 만들기",
+  },
+  {
+    order: 2,
+    title: "프로필 카드 만들기",
+    description:
+      "object 형식의 데이터를 직접 만들어 보고, 그 데이터를 이용해서 프로필 카드 만들기",
+  },
+];
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="h-full w-full flex flex-col relative overflow-hidden">
+      <Header title={"Frontend 과제"} />
+      <main className="w-full h-full flex flex-col overflow-y-auto py-5 px-5 gap-5">
+        {assignments.map((assignment, index) => (
+          <Assignment
+            key={index}
+            title={`${assignment.order}. ${assignment.title}`}
+            href={`/assignments/${assignment.order}`}
+            description={assignment.description}
+          />
+        ))}
+      </main>
+    </div>
+  );
+}
+
+function Assignment({
+  title,
+  href,
+  description,
+}: {
+  title: string;
+  href: string;
+  description?: string;
+}) {
+  return (
+    <Link
+      href={href}
+      className="bg-white border rounded-md p-5 flex flex-col hover-bg-stone-100"
+    >
+      <div className="font-bold mb-2">{title}</div>
+      {description && (
+        <div className="text-sm text-stone-500">{description}</div>
+      )}
+    </Link>
+  );
 }
