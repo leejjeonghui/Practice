57856c324f51e215b79354462b2d420af121f226
jjeonghui <zzhenxxi97@gmail.com>
2024-05-28 18:25:50 +0900

    실습

--- src/app/assignments/1/page.tsx
+++ src/app/assignments/1/page.tsx
@@ -2,27 +2,47 @@ import Header from "@/components/header";
 
 export default function Assignment1() {
   const product = {
-    imageSrc: "https://gdimg.gmarket.co.kr/2004408943/still/300?ver=1712543662",
+    imageSrc: "",
     title: "(15%쿠폰)탱글탱글 대추방울토마토 2kg 4번 소과",
     price: 13900,
   };
+
+  const products = [
+    {imageSrc: "",
+    title: "(15%쿠폰)탱글탱글 대추방울토마토 2kg 4번 소과",
+    price: 13900},
+    {imageSrc: "https://src.hidoc.co.kr/image/lib/2021/6/24/1624517344477_0.jpg",
+    title: "복숭아",
+    price: 15000},
+    {
+    imageSrc: "https://www.nongmin.com/-/raw/srv-nongmin/data2/content/image/2022/02/13/.cache/512/20220213191115.jpg",
+    title: "딸기",
+    price: 25000
+    }
+  ]
+
+ function 상품(props:{imageSrc:string,title:string,price:number}){
+  return(
+    <div className="rounded-md bg-white w-52 h-72 border p-3 gap-1 flex flex-col">
+      {props.imageSrc!=""?<img src={props.imageSrc}></img>:<BlankProductImage/>}
+    <div className="h-12 max-h-12 overflow-hidden">{props.title}</div>
+    <div className="font-bold">{props.price}</div>
+    {/* --- 윗 줄까지 --- */}
+  </div>
+  )
+ }
 
   return (
     <>
       <Header title={"상품 목록 만들기"} hasBack={true} />
       <div className="w-full h-full flex flex-col overflow-y-auto py-5 px-5 gap-5 scrollbar-hide">
         <div className="w-full gap-4 grid grid-cols-2">
-          <div className="rounded-md bg-white w-52 h-72 border p-3 gap-1 flex flex-col">
-            {/* 아래 코드를 완성해 주세요. 태그나 className은 수정하지 않아도 됩니다. */}
-            <img src="" className="rounded-md w-full aspect-square" />
-            <div className="h-12 max-h-12 overflow-hidden">상품명</div>
-            <div className="font-bold">0원</div>
-            {/* --- 윗 줄까지 --- */}
-          </div>
+         {products.map(상품)}
         </div>
       </div>
     </>
   );
+
 }
 
 function BlankProductImage() {
@@ -36,7 +56,7 @@ function BlankProductImage() {
         stroke="currentColor"
         className="size-6"
       >
-        <path
+        <path 
           strokeLinecap="round"
           strokeLinejoin="round"
           d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
@@ -44,4 +64,5 @@ function BlankProductImage() {
       </svg>
     </div>
   );
+  
 }
--- src/app/assignments/2/page.tsx
+++ src/app/assignments/2/page.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/header";
+import { profile } from "console";
 import Image from "next/image";
 
 interface Profile {
@@ -12,16 +13,28 @@ interface Profile {
   famousLines: string[];
 }
 
+const Person = {
+  imageSrc: "",
+  name:"마동석",
+  age:50,
+  nationality:"한국",
+  height:180,
+  weight:100,
+  job:"배우",
+  famousLines:["안녕","하세요"]
+}
+
 export default function Assignment2() {
   return (
     <>
       <Header title={"프로필 카드 만들기"} hasBack={true} />
       <div className="w-full h-full flex flex-col overflow-y-auto py-5 px-5 gap-5">
-        {/* 이 아래에 코드를 작성해 주세요. */}
+        <ProfileCard imageSrc={Person.imageSrc} name={Person.name} age = {Person.age} nationality={Person.nationality} height={Person.height} weight={Person.weight} job={Person.job} famousLines={Person.famousLines}></ProfileCard>
       </div>
     </>
   );
 }
+
 
 function ProfileCard({
   imageSrc,
